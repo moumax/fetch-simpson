65965e7e321926f9636e9c85fd3f1da76e0955e9
Marc LANTOL <marc.lantol@gmail.com>
2022-04-11 12:44:16 +0200

    Je suis content, donc, je commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import DisplayQuote from "./components/DisplayQuote";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [quote, setQuote] = useState("");
+
+  const getQuote = () => {
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      .then((response) => response.data)
+      .then((data) => {
+        setQuote(data[0]);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button type="button" onClick={getQuote}>
+        Get quote
+      </button>
+      <DisplayQuote quote={quote} />
     </div>
   );
 }
---
+++ src/components/DisplayQuote.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function DisplayQuote({ quote }) {
+  return (
+    quote && (
+      <div className="DisplayQuote">
+        <img src={quote.image} alt={quote.character} />
+        <ul>
+          <li>Name: {quote.character}</li>
+
+          <li>Quote: {quote.quote} </li>
+        </ul>
+      </div>
+    )
+  );
+}
+
+export default DisplayQuote;
